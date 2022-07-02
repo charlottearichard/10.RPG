5dadf7e664947cfcf97b5092e3605cc40caec3d9
Charlotte <charlottealiciarichard@gmail.com>
2022-07-02 17:56:59 -0400

    Prototype

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -18,3 +18,22 @@ test("creates a player object", () => {
     expect.arrayContaining([expect.any(Object)])
   );
 });
+
+test("gets player stats as an object", () => {
+  const player = new Player("Dove");
+
+  expect(player.getStatus()).toHaveProperty("potions");
+  expect(player.getStatus()).toHaveProperty("health");
+  expect(player.getStatus()).toHaveProperty("strength");
+  expect(player.getStatus()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dove");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
