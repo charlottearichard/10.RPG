360b0126e01e089db8b42a225c4e551782c26667
Charlotte <charlottealiciarichard@gmail.com>
2022-07-03 14:08:42 -0400

    Enemy

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -66,7 +66,7 @@ test("checks if player is alive", () => {
 
 //subtract players health
 test("subtracts from player's health", () => {
-  const player = new Player("Dave");
+  const player = new Player("Dove");
   const oldHealth = player.health;
 
   player.reduceHealth(5);
@@ -76,4 +76,34 @@ test("subtracts from player's health", () => {
   player.reduceHealth(99999);
 
   expect(player.health).toBe(0);
+});
+
+//Players attack
+test("gets player's attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//  Potion  IN Inventory
+
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dove");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
 });
